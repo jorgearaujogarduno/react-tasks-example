ae601c157d3558623ba3da1559f765a184ee8505
Jorge Araujo <jorgearaujogarduno@gmail.com>
2026-04-21 17:14:06 -0600

    Automatizar

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { ghPages } from 'vite-plugin-gh-pages'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),    
+    ghPages() // Opcionalmente pasa opciones como { branch: 'production' }
   ],
   base: '/react-tasks-example/'
 })
